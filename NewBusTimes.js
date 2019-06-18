012ed66bdf9fea0a19660c940c6d3808823f8c91
Sam Detweiler <sdetweil@gmail.com>
2019-06-17 22:33:09 -0500

    remove unused  scripts and translation loader functions

--- NewBusTimes.js
+++ NewBusTimes.js
@@ -42,46 +42,12 @@ Module.register("NewBusTimes", {
 		callback();
 	},
 
-	// return list of other functional scripts to use, if any (like require in node_helper)
-	getScripts: function() {
-		return
-		[
-			// sample of list of files to specify here, if no files,do not use this routine, or return empty list
-
-			//'script.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-			//'moment.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
-			//this.file('anotherfile.js'), // this file will be loaded straight from the module folder.
-			//'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
-		]
-	},
-
 	// return list of stylesheet files to use if any
 	getStyles: function() {
       Log.log("returning our styleheet")
   return ["NewBusTimes.css"]
-	/*	[
-			// sample of list of files to specify here, if no files, do not use this routine, , or return empty list
 
-			//'script.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-			//'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be avialable in the module folder.
-			//this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
-			//'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
-      		
-		] */
 	},
-
-	// return list of translation files to use, if any
-	getTranslations: function() {
-		return
-		[
-			// sample of list of files to specify here, if no files, do not use this routine, , or return empty list
-
-			// en: "translations/en.json",  (folders and filenames in your module folder)
-			// de: "translations/de.json"
-		]
-	},
-
-
 
 	// only called if the module header was configured in module config in config.js
 	getHeader: function() {
@@ -114,19 +80,16 @@ Module.register("NewBusTimes", {
       setInterval(()=> { this.updateDom(1000)}, 60*1000)
 			this.updateDom(1000)
 		}
-
 	},
 
 	// system notification your module is being hidden
 	// typically you would stop doing UI updates (getDom/updateDom) if the module is hidden
 	suspend: function(){
-
 	},
 
 	// system notification your module is being unhidden/shown
 	// typically you would resume doing UI updates (getDom/updateDom) if the module is shown
 	resume: function(){
-
 	},
   createElement: function(etype, eparent, eclass, data){
     let e = document.createElement(etype)
